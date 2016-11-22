2e53ae5e4a06fc6d08f14700a11537dde923a696
ShrimpKeeper <xophnex@gmail.com>
2016-11-22 19:53:37 +0800

    cleaned up code and added more comments

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -3,29 +3,27 @@
 int main(int argc, const char *argv[])
 {
 	int count, num = 1, fizz_count = 1, buzz_count = 1;
-	if (argc == 1) //if no time is specified then loop endlessly
-	count = -1;
-	else
-	count = atoi(argv[1]);
+	if (argc == 1)
+	count = atoi(argv[1]);//set the amount to print to first argument
 
-	while ((num < count) || (count == -1)) {
-		if (fizz_count <= 2 && buzz_count <= 4) {
+	while ((num < count) || (argc < 1)) {//loop forever or for count
+		if (fizz_count <= 2 && buzz_count <= 4) {//check if regular output
 			std::cout<<num;
 		} else {
-			if (fizz_count > 2) {
+			if (fizz_count > 2) {//check fizz counter
 				std::cout<<"fizz";
 				fizz_count = 0;
 			}
-			if (buzz_count > 4) {
+			if (buzz_count > 4) {//check buzz counter
 				std::cout<<"buzz";
 				buzz_count = 0;
 			}
 		}
+		//increase all counters
 		num++;
 		fizz_count++;
 		buzz_count++;
-		std::cout<<std::endl;
+		std::cout<<std::endl;//add line after each loop
 	}
-
 	return 0;
 }
